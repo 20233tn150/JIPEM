128e21ff7df41bda5856e189350852612d8fc90c
isaacOG145 <20233tn145@utez.edu.mx>
2026-03-26 16:19:52 -0600

    feat: attendance urls

--- backend/config/urls.py
+++ backend/config/urls.py
@@ -3,5 +3,6 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('api/attendance/', include('apps.attendance.urls')),
     path('api/classrooms/', include('apps.classrooms.urls')),
 ]
